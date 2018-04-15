801b09078d5a19a7b72f390eb8807e0061b4ef1e
psanel <prakash.sanel@gmail.com>
2018-04-15 19:28:48 -0400

    Created concrete products.

---
+++ DesignPatternsDemo/DesignPatternsDemo/CreationalDesignPattern/FactoryMethod/Product/Huawei.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsDemo.CreationalDesignPattern.FactoryMethod
+{
+    public class Huawei : IMobile
+    {
+        public decimal MaximumMobilePrice()
+        {
+            decimal _maxPrice = 850;
+            return _maxPrice;
+        }
+
+        public decimal MinimumMobilePrice()
+        {
+            decimal _minPrice = 150;
+            return _minPrice;
+        }
+    }
+}
