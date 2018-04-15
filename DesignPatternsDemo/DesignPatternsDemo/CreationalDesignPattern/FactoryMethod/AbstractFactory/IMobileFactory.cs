0f7295167586bdb7055c91fe81651acc2df07b73
psanel <prakash.sanel@gmail.com>
2018-04-15 19:46:07 -0400

    .

--- DesignPatternsDemo/DesignPatternsDemo/CreationalDesignPattern/FactoryMethod/AbstractFactory/IMobileFactory.cs
+++ DesignPatternsDemo/DesignPatternsDemo/CreationalDesignPattern/FactoryMethod/AbstractFactory/IMobileFactory.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DesignPatternsDemo.CreationalDesignPattern.FactoryMethod
 {
-    interface IMobileFactory
+    public interface IMobileFactory
     {
         IMobile GetMobile();        
     }
